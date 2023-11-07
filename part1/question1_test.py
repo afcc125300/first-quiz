865f10d5c2ead2b80222b728a36d08b7861141d7
afcc125300 <105983661+afcc125300@users.noreply.github.com>
2023-11-07 15:29:04 -0500

    Update question1_test.py

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -1,7 +1,14 @@
 from question1 import get_city_weather
 
-def test_get_city_weather():
-
-  assert get_city_weather("Quito") == "22 degrees and sunny"
-
-  assert get_city_weather("New York") == "14 degrees and rainy"
+def get_city_weather(city):
+  temperature = get_city_temperature(city)
+  if temperature is not None:
+    sky_condition = None
+    if city == "Sao Paulo":
+      sky_condition = "cloudy"
+    elif city == "New York":
+      sky_condition = "rainy"
+    
+    return f"{temperature} degrees and {sky_condition}" if sky_condition is not None else f"{temperature} degrees"
+  else:
+    return "City not found"  # Or handle the error accordingly.
